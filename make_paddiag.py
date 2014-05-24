66a870ad9721127db0704f18f08e57b854a34db5
tk0miya <i.tkomiya@gmail.com>
2014-05-24 21:15:52 +0900

    Adjust image size of paddiag

--- make_paddiag.py
+++ make_paddiag.py
@@ -10,10 +10,10 @@ from blockdiag.utils.fontmap import FontMap
 
 
 PAGE_MARGIN = 32
-NODE_WIDTH = 160
+NODE_WIDTH = 240
 NODE_HEIGHT = 48
 SPAN_WIDTH = 40
-SPAN_HEIGHT = 16
+SPAN_HEIGHT = 24
 
 
 def p(x, y):
@@ -21,20 +21,22 @@ def p(x, y):
               int(PAGE_MARGIN + NODE_HEIGHT * y + SPAN_HEIGHT * y))
 
 
-def box(x, y, height=1):
+def box(x, y, width=1, height=1):
     x, y = p(x, y)
     return Box(x, y,
-               x + NODE_WIDTH,
+               x + NODE_WIDTH * width + SPAN_WIDTH * (width - 1),
                y + NODE_HEIGHT * height + SPAN_HEIGHT * (height - 1))
 
 
 class PADImageDraw(ImageDrawEx):
     def __init__(self, *args, **kwargs):
         super(PADImageDraw, self).__init__(*args, **kwargs)
-        self.font = FontMap().find()
+        self.fontmap = FontMap()
+        self.fontmap.set_default_font('/Library/Fonts/Hiragino Sans GB W3.otf')
+        self.font = self.fontmap.find()
 
     def if_block(self, x, y, stmt):
-        height = self.statement(x + 1, y, stmt.body)
+        width, height = self.statement(x + 1, y, stmt.body)
 
         textbox = box(x, y, height + 0.25).shift(y=NODE_HEIGHT / 3)
         shape = (textbox.topleft, textbox.topright, textbox.right.shift(x=-32),
@@ -46,10 +48,12 @@ class PADImageDraw(ImageDrawEx):
         if stmt.orelse is None:
             height += 1
         else:
-            height += self.statement(x + 1, y + height, stmt.orelse)
+            w, h = self.statement(x + 1, y + height, stmt.orelse)
+            width = max(width, w)
+            height += h
             self.link(x, textbox.bottom.y)
 
-        return height
+        return (width + 1, height)
 
     def while_block(self, x, y, stmt):
         textbox = box(x, y)
@@ -58,7 +62,8 @@ class PADImageDraw(ImageDrawEx):
         self.line((textbox.topleft.shift(x=12), textbox.bottomleft.shift(x=12)), fill='black')
         self.link(x, textbox.right.y)
 
-        return self.statement(x + 1, y, stmt.body)
+        width, height = self.statement(x + 1, y, stmt.body)
+        return (width + 1, height)
 
     def for_block(self, x, y, stmt):
         textbox = box(x, y)
@@ -68,33 +73,40 @@ class PADImageDraw(ImageDrawEx):
         self.line((textbox.topleft.shift(x=12), textbox.bottomleft.shift(x=12)), fill='black')
         self.link(x, textbox.right.y)
 
-        return self.statement(x + 1, y, stmt.body)
+        width, height = self.statement(x + 1, y, stmt.body)
+        return (width + 1, height)
 
     def render(self, tree):
-        self.statement(0, 0, tree.body)
+        return self.statement(0, 0, tree.body)
 
     def statement(self, x, y, statements):
         height = 0
+        width = 0
         for stmt in statements:
             if isinstance(stmt, str):
-                height += self.process(x, y + height, stmt)
+                w, h = self.process(x, y + height, stmt)
             elif isinstance(stmt, ast.If):
-                height += self.if_block(x, y + height, stmt)
+                w, h = self.if_block(x, y + height, stmt)
             elif isinstance(stmt, ast.While):
-                height += self.while_block(x, y + height, stmt)
+                w, h = self.while_block(x, y + height, stmt)
             elif isinstance(stmt, ast.For):
-                height += self.for_block(x, y + height, stmt)
+                w, h = self.for_block(x, y + height, stmt)
+            else:
+                w, h = (0, 0)
+
+            height += h
+            width = max(width, w)
 
         self.baseline(x, y, height)
 
-        return height
+        return (width, height)
 
     def process(self, x, y, text):
         textbox = box(x, y)
         self.rectangle(textbox, outline='black', fill='white')
         self.textarea(textbox, text, self.font, fill='black')
 
-        return 1
+        return (1, 1)
 
     def baseline(self, x, y, height):
         start = box(x, y).shift(y=-SPAN_HEIGHT / 3)
@@ -112,7 +124,11 @@ def main():
 
     path = os.path.splitext(sys.argv[1])[0] + '.png'
     drawer = PADImageDraw(path)
-    drawer.set_canvas_size((512, 512))
+    width, height = drawer.render(tree)
+
+    # reset canvas
+    diagram = box(0, 0, width=width, height=height)
+    drawer.set_canvas_size(diagram.bottomright.shift(PAGE_MARGIN, PAGE_MARGIN))
     drawer.render(tree)
     drawer.save(None, None, 'PNG')
 
